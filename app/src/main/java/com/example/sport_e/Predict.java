ff170dab9499a27b7efe1f9eaf5b4a493f56be7b
jayd512 <jayadevjayesh.512@gmail.com>
2021-02-24 21:53:49 +0530

    predict animation

--- app/src/main/java/com/example/sport_e/Predict.java
+++ app/src/main/java/com/example/sport_e/Predict.java
@@ -2,10 +2,16 @@ package com.example.sport_e;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.Animator;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,6 +24,7 @@ public class Predict extends AppCompatActivity {
     private ImageView pback;
     private Button hi,hd,ai,ad,place;
     private TextView home,away;
+    private LottieAnimationView firework;
     int hscore = 0;
     int awscore = 0;
 
@@ -32,6 +39,8 @@ public class Predict extends AppCompatActivity {
         home = findViewById(R.id.predict_hometeam_score);
         away = findViewById(R.id.predict_awayteam_score);
         place = findViewById(R.id.placeprediction_btn);
+        firework = findViewById(R.id.predict_animation);
+
 
         hi.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -73,6 +82,42 @@ public class Predict extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Toast.makeText(Predict.this, "Prediction Is placed!!", Toast.LENGTH_SHORT).show();
+                firework.playAnimation();
+
+                firework.addAnimatorListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Handler handler = new Handler();
+                        handler.postDelayed(new Runnable() {
+                            @Override
+                            public void run() {
+                                Animation fadeOut = new AlphaAnimation(1, 0);
+                                fadeOut.setInterpolator(new AccelerateInterpolator()); //and this
+                                fadeOut.setStartOffset(1000);
+                                fadeOut.setDuration(500);
+                                AnimationSet animationSet = new AnimationSet(false);
+                                animationSet.addAnimation(fadeOut);
+                                firework.setAnimation(animationSet);
+                                firework.setVisibility(View.GONE);
+                            }
+                        },2000);
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
             }
         });
 
